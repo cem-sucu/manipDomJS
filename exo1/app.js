69a34918589575130a13043864f9eed62eb16ab6
cem-sucu <cems.cems.17@gmail.com>
2023-07-20 09:48:11 +0200

    box cliquable qui affiche un prompt

--- exo1/app.js
+++ exo1/app.js
@@ -1,6 +1,13 @@
 const box = document.createElement("div");
 box.classList.add("box");
+box.textContent = "click sur moi "
 
 const carre = document.querySelector(".carre");
 carre.appendChild(box);
+
+
+carre.addEventListener("click", function(){
+    alert("Class : carre \n - Background-color : rgb(136,174,136) \n - Color : rgb(255, 255, 255) \n - Height : 200 \n - Width : 200 \n - Display : flex \n - Display: 'Times New Roman' (40px)");
+});
+
 
