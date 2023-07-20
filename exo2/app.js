0121eb4c3a11e98ce0de48b1e20b424f1de0feb4
cem-sucu <cems.cems.17@gmail.com>
2023-07-20 12:02:32 +0200

    function animation click remove add

--- exo2/app.js
+++ exo2/app.js
@@ -8,12 +8,21 @@ const carre = document.querySelector(".carre");
 //mise en place de plusieurs boites
 for(let i = 1; i <= 4; i++){
     const newbox = box.cloneNode();
+    newbox.innerText = i;
     carre.appendChild(newbox);
 
 
     // funcion qui peremt de cliquer sur chaque box generer indépendament et qui active ls css dessus
     newbox.addEventListener("click", function(){
-    newbox.classList.add("box-valid");
+    
+        // si il y a la class box-valid
+        if(newbox.classList.contains("box-valid")){
+            //retire box-valid
+            newbox.classList.remove("box-valid");
+        } else {
+            // sinon ajoute box-valid
+            newbox.classList.add("box-valid");
+        };
 });
 }
 
