73130cb3ae0e7bdc3b521ae65a2b4b4240b99216
cem-sucu <cems.cems.17@gmail.com>
2023-07-22 12:31:52 +0200

    mise en place de la fucntion boxClicker et de la function joueurActuel et l'appel de la fucntion boxClciker

--- exo3/app.js
+++ exo3/app.js
@@ -1,34 +1,44 @@
 //mise en place de ma box
+//dans ma div class .carre dans le html je cree avec document.creatElement une nouvelle <div>
 const box = document.createElement("div");
+//la <div> que je viens de creer je lui ajoute un nom de class box donc -> <div class="box">
 box.classList.add('box');
+// ici je selectionne avec document.querySelector la <div class="carre"> qui existe deja dans le html
 const carre = document.querySelector(".carre");
 
+//je creer un array [] vide que j'appel grid
+const grid = [];
+
+
+
+function boxClicker(event){
+    // console.log(event);
+    // console.log(event.target);
+    const box = event.target;
+    console.log(carre);
+    console.log(carre.children);
+    const index = Array.from(carre.children).indexOf(box);
+
+    if(grid[index] === undefined){
+        const joueurA = joueurActuel();
+        grid[index] = joueurA;
+        // console.log(grid[index]);
+        // console.log(grid);
+        box.innerText = joueurA;
+    }
+}
+
+function joueurActuel(){
+    // console.log(grid);
+    // console.log(grid.filter);
+    const cassClicker = grid.filter(Boolean).length;
+    return cassClicker %2 === 0 ? "X" : "O";
+}
 
 // creation de 9 box pour le morpion
 for( let i = 1; i <= 9; i++){
     const newbox = box.cloneNode();
+    newbox.addEventListener('click', boxClicker);
     carre.appendChild(newbox);
-    
-
-    //function click morpion X ou O
-    newbox.addEventListener("click", function(){ 
-        if(!newbox.classList.contains("box")){
-            newbox.innerText = "X";
-            newbox.style.color= "red";
-        } else if(newbox.classList.contains("box")){
-            newbox.innerText = "O";
-            newbox.style.color = "green";
-        };
-        newbox = newbox === "O" ? "O" : "X"; 
-    });
-
-
-    
-
-
 
 };
-
-
-
-
