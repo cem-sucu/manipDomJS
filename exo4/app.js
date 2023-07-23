f403f1607cae711922797e341de504ad8379dd0d
cem-sucu <cems.cems.17@gmail.com>
2023-07-23 13:34:20 +0200

    creartion de la function boutonPress qui a chaque press de la touche de bas ajoute une nvelle box

--- exo4/app.js
+++ exo4/app.js
@@ -3,3 +3,13 @@ box.classList.add('box');
 
 const carre = document.querySelector(".carre");
 carre.appendChild(box);
+
+
+function boutonPresse(event){
+    if(event.keyCode === 40){
+        const newBox = box.cloneNode();
+        carre.appendChild(newBox);
+    };
+};
+
+document.addEventListener('keydown', boutonPresse);
